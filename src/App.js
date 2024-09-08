085366e910fb2daa574e5ed4f527ea9b788319ab
MEDOYED <mmax26072005@gmail.com>
2024-09-08 18:55:03 +0200

    split App. Add new Costs component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import CostItem from "./components/CostItem";
+import Cost from "./components/Costs";
+// import CostItem from "./components/CostItem";
+import Costs from "./components/Costs";
 
 function App() {
   const costs = [
@@ -22,21 +24,7 @@ function App() {
   return (
     <div>
       <h1>Начнем изучение React!</h1>
-      <CostItem
-        date={costs[0].date}
-        description={costs[0].description}
-        amount={costs[0].amount}
-      ></CostItem>
-      <CostItem
-        date={costs[1].date}
-        description={costs[1].description}
-        amount={costs[1].amount}
-      ></CostItem>
-      <CostItem
-        date={costs[2].date}
-        description={costs[2].description}
-        amount={costs[2].amount}
-      ></CostItem>
+      <Costs costs={costs} />
     </div>
   );
 }
