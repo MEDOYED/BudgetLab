23009aef67a6d8869147c11590eb0ac6ebc8876f
MEDOYED <mmax26072005@gmail.com>
2024-09-08 14:01:18 +0200

    fixed the first component --> CostItem

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,6 +1,15 @@
-function CostItem() {
+import './CostItem.css';
+
+function CostItem(props) {
+
   return (
-    <h2>Елемент розходу</h2>
+    <div className='cost-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='cost-item__description'>
+        <h2>{props.description}</h2>
+        <div className='cost-item__price'>${props.amount}</div>
+      </div>
+    </div>
   );
 }
 
