25e62c27381862f02013f8ef627676ece7e8968c
MEDOYED <mmax26072005@gmail.com>
2024-09-11 09:05:31 +0200

    Adding an Entry Form

---
+++ src/components/NewCost/CostForm.js
@@ -0,0 +1,27 @@
+import "./CostForm.css";
+
+const ConstForm = () => {
+  return (
+    <form>
+      <div className="new-cost__controls">
+        <div className="new-cost__control">
+          <label>Назва</label>
+          <input type="text"></input>
+        </div>
+        <div className="new-cost__control">
+          <label>Сума</label>
+          <input type="text" min="0.01" step="0.01"></input>
+        </div>
+        <div className="new-cost__control">
+          <label>Дата</label>
+          <input type="text" min="2019-01-01" step="2025-01-01"></input>
+        </div>
+        <div className="new-cost__actions">
+          <button type="submit">Добавити витрату</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ConstForm;
