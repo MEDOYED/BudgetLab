9e211b12f16bac1b823938f4a7e47680451547b0
MEDOYED <mmax26072005@gmail.com>
2024-09-20 21:47:41 +0200

    Add diagram of costs

--- src/components/Costs/Costs.jsx
+++ src/components/Costs/Costs.jsx
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
 import React, { useState } from "react";
 import CostList from "./CostList";
+import CostsDiagram from "./CostsDiagram";
 
 const Costs = props => {
   const [selectedYear, setSelectedYear] = useState("2021");
@@ -19,6 +20,7 @@ const Costs = props => {
     <div>
       <Card className="costs">
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+        <CostsDiagram costs={filteredCosts} />
         <CostList costs={filteredCosts} />
         {/* {filteredCosts.length === 0 && <p>В ЦЬОМУ РОЦІ НЕМА НІЯКИХ ВИТРАТ!</p>}
         {filteredCosts.length > 0 &&
